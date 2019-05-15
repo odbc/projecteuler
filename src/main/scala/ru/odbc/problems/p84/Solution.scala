7145ea43cefd6f8f69c667fb9685a32038300190
odbc <odbc@yandex.ru>
2019-05-15 22:06:56 +0300

    Solved 84 problem

--- src/main/scala/ru/odbc/problems/p84/Solution.scala
+++ src/main/scala/ru/odbc/problems/p84/Solution.scala
@@ -2,7 +2,7 @@ package ru.odbc.problems.p84
 
 object Solution extends App {
 
-  val dl = 6      // diceLimit
+  val dl = 4      // diceLimit
 
   val dices = for {
     first  <- 1 to dl
@@ -17,8 +17,7 @@ object Solution extends App {
 
   val fromProbs = for {
     source <- 0 to 39
-  } yield (source, {
-    dices.map { case (dice1, dice2) =>
+  } yield dices.map { case (dice1, dice2) =>
       val doubles3Prop    = if (dice1 == dice2) 1.0 / (dl * dl) else 0.0
       val notDoubles3Prop = 1 - doubles3Prop
 
@@ -84,81 +83,32 @@ object Solution extends App {
         }
       }
     }
-  })
 
-  println(fromProbs)
-
-  /*  for {
+  val matrix = for {
     target <- 0 to 39
-    source <- 0 to 39
-  } {
-    val sourceProps = dices.map { case (dice1, dice2) =>
-      val doubles3Prop    = if (dice1 == dice2) 1.0 / (dl * dl) else 0.0
-      val notDoubles3Prop = 1 - doubles3Prop
-
-      val turn    = dice1 + dice2
-      val endTurn = (source + turn) % 40
-
-      val endsProp =
-        if (endTurn == 30) Map(10 -> notDoubles3Prop)
-        else if (CCs.contains(endTurn)) Map(
-          0  -> notDoubles3Prop * 1.0 / 16,
-          10 -> notDoubles3Prop * 1.0 / 16,
-          endTurn -> notDoubles3Prop * 14.0 / 16,
-        )
-        else if (endTurn == 7) Map(
-          0  -> (notDoubles3Prop * 1.0 / 16),
-          10 -> (notDoubles3Prop * 1.0 / 16),
-          11 -> (notDoubles3Prop * 1.0 / 16),
-          24 -> (notDoubles3Prop * 1.0 / 16),
-          39 -> (notDoubles3Prop * 1.0 / 16),
-          5  -> (notDoubles3Prop * 1.0 / 16),
-          15 -> (notDoubles3Prop * 2.0 / 16),
-          12 -> (notDoubles3Prop * 1.0 / 16),
-          4  -> (notDoubles3Prop * 1.0 / 16),
-          7  -> (notDoubles3Prop * 6.0 / 16),
-        )
-        else if (endTurn == 22) Map(
-          0  -> (notDoubles3Prop * 1.0 / 16),
-          10 -> (notDoubles3Prop * 1.0 / 16),
-          11 -> (notDoubles3Prop * 1.0 / 16),
-          24 -> (notDoubles3Prop * 1.0 / 16),
-          39 -> (notDoubles3Prop * 1.0 / 16),
-          5  -> (notDoubles3Prop * 1.0 / 16),
-          25 -> (notDoubles3Prop * 2.0 / 16),
-          28 -> (notDoubles3Prop * 1.0 / 16),
-          19 -> (notDoubles3Prop * 1.0 / 16),
-          22 -> (notDoubles3Prop * 6.0 / 16),
-        )
-        else if (endTurn == 36) Map(
-          0  -> (notDoubles3Prop * (1.0 / 16 + 1.0 / 256)),
-          10 -> (notDoubles3Prop * (1.0 / 16 + 1.0 / 256)),
-          11 -> (notDoubles3Prop * 1.0 / 16),
-          24 -> (notDoubles3Prop * 1.0 / 16),
-          39 -> (notDoubles3Prop * 1.0 / 16),
-          5  -> (notDoubles3Prop * 3.0 / 16),
-          12 -> (notDoubles3Prop * 1.0 / 16),
-          33 -> (notDoubles3Prop * 14.0 / 256),
-          36 -> (notDoubles3Prop * 6.0 / 16),
-        )
-        else
-          Map(endTurn -> notDoubles3Prop)
-
-      val resProps = endsProp.map { case (t, v) => (t, if (t == 10) v + doubles3Prop else v) }
-      resProps.getOrElse(target, 0.0)
-    }
-    equation(target)(source) = sourceProps.sum / (dl * dl) - (if (source == target) 1.0 else 0.0)
-  }
-
-  equation(0) = equation(0).map(_ + 1)
-  equation.foreach(row => println(row.map(f => f"$f%1.3f").mkString(" ")))*/
-
-  /*println(equation.head.map(_ + 1).mkString(" ") + " 1")
-  equation.tail.foreach(row => println(row.mkString(" ") + " 0"))
-  println(equation.map(_.toList).toList)*/
+  } yield
+    fromProbs.zipWithIndex
+      .map { case (m, i) =>
+        val prob = m.getOrElse(target, 0.0)
+        if (i == target) prob - 1 else prob
+      } ++ Vector(0.0)
 
   /**
-    * https://matrixcalc.org/en/slu.html
+    * Print system of linear equations
     */
 
+  /**
+    * Add equation - sum of all probabilities equals 1
+    */
+  println(matrix.head.map(_ + 1).mkString(" ") + " 1")
+
+  matrix.tail.foreach(row => println(row.mkString(" ")))
+
+  /**
+    * Solve system on
+    * https://matrixcalc.org/en/slu.html
+    * and get three max
+    */
+
+  println("101524")
 }
