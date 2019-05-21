e820a6512aba8e7617d76d269afb30757fb939ad
odbc <simonyan@amulex.ru>
2019-05-21 18:55:59 +0300

    Not ended 90 problem

--- src/main/scala/ru/odbc/problems/p90/Solution.scala
+++ src/main/scala/ru/odbc/problems/p90/Solution.scala
@@ -2,4 +2,26 @@ package ru.odbc.problems.p90
 
 object Solution extends App {
 
+  val arrangements = (0 to 9).combinations(6)
+    .map(_.toSet)
+    .map(s => if (s.contains(6)) s + 9 else if (s.contains(9)) s + 6 else s )
+    .toList
+    .distinct
+
+  val result = (for {
+    left  <- arrangements
+    right <- arrangements
+    if ((left.contains(0) && right.contains(1)) || (right.contains(0) && left.contains(1))) &&
+       ((left.contains(0) && right.contains(4)) || (right.contains(0) && left.contains(4))) &&
+       ((left.contains(0) && right.contains(9)) || (right.contains(0) && left.contains(9))) &&
+       ((left.contains(1) && right.contains(6)) || (right.contains(1) && left.contains(6))) &&
+       ((left.contains(2) && right.contains(5)) || (right.contains(2) && left.contains(5))) &&
+       ((left.contains(3) && right.contains(6)) || (right.contains(3) && left.contains(6))) &&
+       ((left.contains(4) && right.contains(9)) || (right.contains(4) && left.contains(9))) &&
+       ((left.contains(6) && right.contains(4)) || (right.contains(6) && left.contains(4))) &&
+       ((left.contains(8) && right.contains(1)) || (right.contains(8) && left.contains(1)))
+  } yield Set(left, right)).distinct
+
+  println(result.size)
+
 }
