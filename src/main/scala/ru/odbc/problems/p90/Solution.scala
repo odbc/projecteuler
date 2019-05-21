8f9fd7d841693a10c238352edd27ef0b9c89e133
odbc <odbc@yandex.ru>
2019-05-22 00:23:16 +0300

    Solved 90 problem

--- src/main/scala/ru/odbc/problems/p90/Solution.scala
+++ src/main/scala/ru/odbc/problems/p90/Solution.scala
@@ -2,26 +2,33 @@ package ru.odbc.problems.p90
 
 object Solution extends App {
 
-  val arrangements = (0 to 9).combinations(6)
-    .map(_.toSet)
-    .map(s => if (s.contains(6)) s + 9 else if (s.contains(9)) s + 6 else s )
-    .toList
-    .distinct
+  val arrangements = (0 to 9).combinations(6).map(_.toSet).toList.distinct
 
   val result = (for {
     left  <- arrangements
     right <- arrangements
-    if ((left.contains(0) && right.contains(1)) || (right.contains(0) && left.contains(1))) &&
-       ((left.contains(0) && right.contains(4)) || (right.contains(0) && left.contains(4))) &&
-       ((left.contains(0) && right.contains(9)) || (right.contains(0) && left.contains(9))) &&
-       ((left.contains(1) && right.contains(6)) || (right.contains(1) && left.contains(6))) &&
-       ((left.contains(2) && right.contains(5)) || (right.contains(2) && left.contains(5))) &&
-       ((left.contains(3) && right.contains(6)) || (right.contains(3) && left.contains(6))) &&
-       ((left.contains(4) && right.contains(9)) || (right.contains(4) && left.contains(9))) &&
-       ((left.contains(6) && right.contains(4)) || (right.contains(6) && left.contains(4))) &&
-       ((left.contains(8) && right.contains(1)) || (right.contains(8) && left.contains(1)))
+    canDisplay01 = (left.contains(0) && right.contains(1)) || (right.contains(0) && left.contains(1))
+    canDisplay04 = (left.contains(0) && right.contains(4)) || (right.contains(0) && left.contains(4))
+    canDisplay09 =
+      (left.contains(0) && (right.contains(9) || right.contains(6))) ||
+      (right.contains(0) && (left.contains(9) || left.contains(6)))
+    canDisplay16 =
+      (left.contains(1) && (right.contains(6) || right.contains(9))) ||
+      (right.contains(1) && (left.contains(6) || left.contains(9)))
+    canDisplay25 = (left.contains(2) && right.contains(5)) || (right.contains(2) && left.contains(5))
+    canDisplay36 =
+      (left.contains(3) && (right.contains(6) || right.contains(9))) ||
+      (right.contains(3) && (left.contains(6) || left.contains(9)))
+    canDisplay49 =
+      (left.contains(4) && (right.contains(9) || right.contains(6))) ||
+      (right.contains(4) && (left.contains(9) || left.contains(6)))
+    canDisplay64 =
+      ((left.contains(6) || left.contains(9)) && right.contains(4)) ||
+      ((right.contains(6) || right.contains(9)) && left.contains(4))
+    canDisplay81 = (left.contains(8) && right.contains(1)) || (right.contains(8) && left.contains(1))
+    if canDisplay01 && canDisplay04 && canDisplay09 && canDisplay16 && canDisplay25 &&
+       canDisplay36 && canDisplay49 && canDisplay64 && canDisplay81
   } yield Set(left, right)).distinct
 
   println(result.size)
-
 }
